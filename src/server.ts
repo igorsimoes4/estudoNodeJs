a9f2cc246b614abbab72d626467694f4e844f2bd
Igor Simoes da Silvera <igor01silveira@gmail.com>
2023-08-22 22:51:28 -0300

    Aprendendo o conceito de rota dinamica no Node

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,14 @@ server.get('/', (req: Request, res: Response)=>{
     res.send('Ola mundo!');
 });
 
+server.get('/noticia/:slug', (req: Request, res: Response)=>{
+    let slug: string = req.params.slug;
+    res.send(`Noticia: ${slug}`);
+});
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response)=>{
+    let {origem, destino} = req.params;
+    res.send(`Procurando voos de: ${origem.toUpperCase()} até ${destino.toUpperCase()}`);
+});
+
 server.listen(3000);
