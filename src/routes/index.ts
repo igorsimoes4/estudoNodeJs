dcd2a055f7d6fdf3646111f1dd1299ba03e68008
Igor Simoes da Silvera <igor01silveira@gmail.com>
2023-08-28 23:51:50 -0300

    Entendendo como passar variavel para o template

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,16 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response)=>{
-    res.render('home');
+
+    let user = {
+        name : 'Igor',
+        lastname: 'Simões da Silveira',
+        age: 23
+    };
+
+    res.render('home', {
+        user
+    });
 });
 
 router.get('/contato', (req: Request, res: Response)=>{
