d8041ac3a1e3d22ff022cf3bc66c995ae475c2d5
Igor Simoes da Silvera <igor01silveira@gmail.com>
2023-08-28 23:43:02 -0300

    Instalando e Configurando Template Mustache

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
+
+// Importando o Template Engine 
+import mustache from 'mustache-express';
+
+// Importando as Rotas
 import mainRoutes from './routes/index';
 import noticiaRoutes from './routes/noticia';
 import vooRoutes from './routes/voo';
-import path from 'path';
+
 
 const server = express();
+
+// Configurando o Template Mustache //
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
 
 // Definindo rota para arquivo Publicos //
 server.use(express.static( path.join(__dirname, '../public') ));
