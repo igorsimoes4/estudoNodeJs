e7b1d4248928a66b9209b266ebc31940bc61c051
Igor Simoes da Silvera <igor01silveira@gmail.com>
2023-08-30 21:21:45 -0300

    Recebendo dados pela URL metodo GET

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response)=>{
+router.get('/', ( req: Request, res: Response )=>{
 
     let age: number = 90;
     let showOld: boolean = false;
@@ -35,12 +35,43 @@ router.get('/', (req: Request, res: Response)=>{
     });
 });
 
-router.get('/contato', (req: Request, res: Response)=>{
+router.get('/contato', ( req: Request, res: Response )=>{
     res.render('pages/contato');
 });
 
-router.get('/sobre', (req: Request, res: Response)=>{
+router.get('/sobre', ( req: Request, res: Response )=>{
     res.render('pages/sobre');
+});
+
+router.get('/nome', ( req: Request, res: Response )=>{
+
+    let nome: string = req.query.nome as string;
+    let idade: string = req.query.idade as string;
+
+    res.render('pages/nome', {
+        nome,
+        idade
+    });
+});
+
+router.get('/idade', ( req: Request, res: Response ) => {
+    let idade: number = 0;
+    let mostraridade: boolean = false;
+
+    if( req.query.ano ) {
+        let anoNascimento: number = parseInt(req.query.ano as string);
+        let anoAtual: number = new Date().getFullYear();
+
+        idade = anoAtual - anoNascimento;
+        mostraridade = true;
+
+    }   
+    
+
+    res.render('pages/idade', {
+        idade,
+        mostraridade
+    });
 });
 
 export default router;
